c19c571352e42b6ffe3e763456bef8a52ce113ce
wmachimbira <wmachimbira@gmail.com>
2025-03-18 11:38:20 +0900

    integers in python

---
+++ lesson2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# this is a single line comment
+
+
+"""
+
+This is a multiple 
+line comment
+"""
+
+# data types and variables
+
+#intergers positive or negative whole numbers
+
+number = 5
+
+print (number)
+
+# arithmetic operations on integers
+
+num1 = 45
+num2 = 33
+
+sum =(num1 + num2)
+diff =(num1 - num2)
+modulus = (num1 % num2)
+
+print (f"{num1} + {num2} = {sum}") 
+print (f"{num1} - {num2} = {diff}") 
+print (f"{num1} % {num2} = {modulus}") 
